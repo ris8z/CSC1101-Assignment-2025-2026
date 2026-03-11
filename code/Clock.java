78d072716a58b812ecca7ea1358c309faf81fb2c
giuseppe esposito <giuseppe592999@gmail.com>
2026-03-11 03:31:16 +0000

    just suggestion for now, still studing the best way to approach the problem

--- code/Clock.java
+++ code/Clock.java
@@ -16,6 +16,11 @@ public class Clock implements Runnable {
         return INSTANCE.tick;
     }
 
+    // SUGGESTION: we should use and observer pattern (clock ground truth that notify when the time it's passed for all the others)
+    // because now we'are using a sleep for the cloack
+    // and a different sleep when we want to put a thread on aside,
+    // the result of 3 sleep of 10 sec could not be the same of 1 sleep of 30 sec
+    // bc the os taks a bit of time to wake up thread
     public static void sleepTicks(long ticks) {
         if (ticks <= 0) return;
         try {
