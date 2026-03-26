6d19a4111f5b8cbf9479ee0ed0979411753fcd77
giuseppe esposito <giuseppe592999@gmail.com>
2026-03-26 04:59:08 +0000

    full 3 simulation working

--- test.py
+++ test.py
@@ -26,7 +26,13 @@ def update_properties(overrides):
             if not modified:
                 f.write(line)
 
-def run_simulation_and_parse():
+def run_simulation():
+    """
+    Run just 1 simulation (let it run just for 3 sec then kill it)
+    You need to set tick_ms to 1, otherwise the data captured is going to be too little
+
+    RETURN a 3-tuple (pickers_average_wait_time, stockers_average_wait_time, number_of_picks_done)
+    """
     out = ""
     try:
         result = subprocess.run(['java', '-cp', 'code', 'Warehouse'], capture_output=True, text=True, timeout=3)
@@ -37,21 +43,22 @@ def run_simulation_and_parse():
             out = out.decode('utf-8', errors='ignore')
 
     if not out:
-        print("No char was captured! Java probaly exploded")
-        return 0, 0
+        print("No char was captured! Java probaly exploded or you did not compile try to run: make")
+        return 0, 0, 0
 
     picker_ticks = re.findall(r'event=pick_done.*waited_ticks=(\d+)', out)
     stocker_ticks = re.findall(r'event=stocker_load.*waited_ticks=(\d+)', out)
-    
+
     p_avg = sum(int(t) for t in picker_ticks) / len(picker_ticks) if picker_ticks else 0
     s_avg = sum(int(t) for t in stocker_ticks) / len(stocker_ticks) if stocker_ticks else 0
+    p_cnt = len(picker_ticks)
     
-    return p_avg, s_avg
+    return p_avg, s_avg, p_cnt 
 
 def experiment_1_tradeoff():
-    """ This experiemnt is too try to prove that we can't increase performance infiitly just by adding more threads
-        We keep picker and trolleys fixed, and we change the number of stocker between 1 to 15:
-        creating 15 simulation that we run just for 3 second.
+    """ 
+    This test is too try to prove that we can't increase performance infinitly just by adding more threads
+    We keep picker and trolleys fixed, and we change the number of stocker between 1 to 15 to see if waiting time decrease
     """
     base_settings = {
         'tick_ms': '1',
@@ -60,38 +67,136 @@ def experiment_1_tradeoff():
     }
     update_properties(base_settings)
 
-    print("-----------------------------------------------------")
-    print("Starting Experiment 1: fixed Picker, Stocker between [1-16]")
-    print("We start with 1 stocker up to 15 to see how the wating times changes")
-    print("config: ", base_settings)
-    print("-----------------------------------------------------")
-    
     stocker_counts = list(range(1, 16))
     picker_wait_results = []
     stocker_wait_results = []
+
+    print("Starting Experiment 1:")
+
+    # =================== running simulations ========================================
+    print("fixed config: ", base_settings)
+    print("variable config: {'num_stockers': '[1-15]'}")
     
     for s in stocker_counts:
         update_properties({'num_stockers': str(s)})
-        p_avg, s_avg = run_simulation_and_parse()
+        p_avg, s_avg, _ = run_simulation()
         
         picker_wait_results.append(round(p_avg, 2))
         stocker_wait_results.append(round(s_avg, 2))
         
         print(f"Stockers: {s} | AVG Picker Wait: {p_avg} | AVG Stocker Wait: {s_avg}")
         
+    # =================== Results ========================================
     print("\nResults:")
     print(f"Number of Stockers = {stocker_counts}")
     print(f"AVG Picker Wait    = {picker_wait_results}")
     print(f"AVG Stocker Wait   = {stocker_wait_results}")
 
+
+def experiment_2_starvation():
+    """
+    In this test we try to break the logic of our unfair trolley resource (we reserve 1 trolley always to stockers)
+    It is divided in the part:
+
+    1) We first try to get starvation on Pickers (because of a lot of stockers and few trolleys with no breaks)
+    2) Then We add breaks to see if they can save our poor Pickers from stravation
+    """
+    base_settings = {
+        'tick_ms': '1',
+        'num_pickers': '1',
+        'num_trolleys': '2',
+        'stocker_break_duration': '0',
+        'delivery_probability': '0.015',
+    }
+    update_properties(base_settings)
+
+    stocker_counts = list(range(2, 21, 2)) 
+    picker_throughput_no_breaks = []
+    picker_throughput_with_breaks = []
+
+    print("Starting Experiment 2:")
+
+    # ================== Part 1 with no breaksS ========================
+    print("\nPart 1, no breaks expected starvation of Pickers")
+    print("fixed config: ", base_settings)
+    print("variable config: {'num_stockers': '[2-20]'} 2..4..6..20")
+
+    for s in stocker_counts:                            
+        update_properties({'num_stockers': str(s)})
+        _, _, p_cnt = run_simulation()
+        picker_throughput_no_breaks.append(round(p_cnt, 2))
+        print(f"Stockers: {s} | Total Picks completed: {p_cnt}")
+
+    # ================= Part 2 with breaks ==============================
+    base_settings['stocker_break_duration'] = '150'
+    base_settings['stocker_break_interval_min'] = '10' 
+    base_settings['stocker_break_interval_max'] = '20'
+    update_properties(base_settings)
+
+    print("\nPart 2, with breaks (hopefully no starvation for Pickers")
+    print("fixed config: ", base_settings)
+    print("variable config: {'num_stockers': '[2-20]'} 2..4..6..20")
+
+    for s in stocker_counts:                            
+        update_properties({'num_stockers': str(s)})
+        _, _, p_cnt = run_simulation()
+        picker_throughput_with_breaks.append(round(p_cnt, 2))
+        print(f"Stockers: {s} | Total Picks completed: {p_cnt}")
+
+    # =================== Results ========================================
+    print("\nResults:")
+    print(f"X_Stockers                      = {stocker_counts}")
+    print(f"Y_picker_throughput (NO Breaks) = {picker_throughput_no_breaks}")
+    print(f"Y_picker_throughput (BREAKS)    = {picker_throughput_with_breaks}\n")
+
+def experiment_3_trolley():
+    """
+    This test is to understand which is the better value of trolleys and if it is related to the fomula k = [S + P] / 2.
+    We keep fixed: 5 Pickers, 5 Stockers and we try a range of trolly values from 2 to 10.
+    """
+    base_settings = {
+        'tick_ms': '1',
+        'num_pickers': '5',
+        'num_stockers': '5',
+        'delivery_probability': '0.01',
+        'initial_boxes_per_section': '0'            # this is because if there are already box in the section picker waiting time get corrputed by the first attempt
+    }
+    update_properties(base_settings)
+
+    trolley_counts = list(range(2, 11))
+    picker_throughput_results = []
+    picker_wait_results = []
+    stocker_wait_results = []
+
+    print("Starting Experiment 3:")
+
+    # =================== running simulations ========================================
+    for t in trolley_counts:
+        update_properties({'num_trolleys': str(t)})
+        p_avg, s_avg, p_cnt = run_simulation()
+
+        picker_throughput_results.append(p_cnt)
+        picker_wait_results.append(round(p_avg, 2))
+        stocker_wait_results.append(round(s_avg, 2))
+        
+        print(f"Trolleys: {t} | Picks: {p_cnt} | AVG Picker Wait: {p_avg} | AVG Stocker Wait: {s_avg}")
+
+    # =================== Results ========================================
+    print("\nResults Experiment 3:")
+    print(f"X_Trolleys          = {trolley_counts}")
+    print(f"Y_Picker_Throughput = {picker_throughput_results}")
+    print(f"Y_Picker_Wait       = {picker_wait_results}")
+    print(f"Y_Stocker_Wait      = {stocker_wait_results}\n")
+
 def main():
     original_config = read_original_config()
     
     try:
-        experiment_1_tradeoff()
+        #experiment_1_tradeoff()
+        #experiment_2_starvation()
+        experiment_3_trolley()
     finally:
         restore_config(original_config)
-        print("\n End of the testing")
 
 if __name__ == '__main__':
     main()
